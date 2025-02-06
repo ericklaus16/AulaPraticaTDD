5217cc478fd58981c65f2e08c57855a191ef849e
Eric Klaus <ericklaus16@gmail.com>
2025-02-06 20:14:45 -0300

    adiciona classes Money, Dollar e Franc com implementação de multiplicação e testes de igualdade

---
+++ Franc.java
@@ -0,0 +1,10 @@
+package pes;
+
+class Franc extends Money {   			
+    Franc(int amount) {      
+       this.amount= amount;
+    }					
+    Franc times(int multiplier)  {      
+        return new Franc(amount * multiplier);					
+    }   			
+}
