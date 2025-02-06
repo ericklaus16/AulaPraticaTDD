33792ba322dc81393d17d09ce8171224a58bb907
Eric Klaus <ericklaus16@gmail.com>
2025-02-06 20:20:43 -0300

    refatora classes Money, Dollar e Franc para implementar método times como abstrato e adiciona métodos de fábrica

--- Franc.java
+++ Franc.java
@@ -2,9 +2,9 @@ package pes;
 
 class Franc extends Money {   			
     Franc(int amount) {      
-       this.amount= amount;
+       this.amount = amount;
     }					
-    Franc times(int multiplier)  {      
+    Money times(int multiplier)  {      
         return new Franc(amount * multiplier);					
     }   			
 }
